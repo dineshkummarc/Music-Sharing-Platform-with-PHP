a261ae18fb5d61123462db6c6fbf649cc6cb0b01
Dinesh Kumar <mapmydestination@gmail.com>
2024-10-13 17:42:01 +0530

    updated

--- config.php
+++ config.php
@@ -8,22 +8,20 @@
 // | DeepSound - The Ultimate PHP Sound Sharing Platform
 // | Copyright (c) 2019 DeepSound. All rights reserved.
 // +------------------------------------------------------------------------+
-header("Location: ./install");
-exit();
 // MySQL Hostname
 $sql_db_host = "localhost";
 // MySQL Database User
-$sql_db_user = "";
+$sql_db_user = "root";
 // MySQL Database Password
 $sql_db_pass = "";
 // MySQL Database Name
-$sql_db_name = "";
+$sql_db_name = "MusicSharingPlatform";
 
 // Site URL
-$site_url = ""; // e.g (http://example.com)
+$site_url = "http://localhost/Music-Sharing-Platform-with-PHP/"; // e.g (http://example.com)
 
 $developer_mode = 0;
 
 // Purchase code
-$purchase_code = ""; // Your purchase code, don't give it to anyone.
+$purchase_code = "123"; // Your purchase code, don't give it to anyone.
 ?>
